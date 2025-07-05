f7cd2e3c7dfb7ff46d40ef76bf4299759822659e
tennis <kumarteneesh@gmail.com>
2025-07-05 20:46:53 +0530

    added env vars

---
+++ config/env.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const requiredEnvVars = ["PORT", "MONGO_URI", "JWT_SECRET", "SESSION_SECRET", "BREVO_API_KEY","SENDER_EMAIL"]
+
+requiredEnvVars.forEach( (key) => {
+    if(!ProcessingInstruction.env[key]){
+        throw new Error(`❌ Missing required environment variable: ${key}`);
+    }
+} )
+
+export default {
+  port: process.env.PORT || 5000,
+  mongoUri: process.env.MONGO_URI,
+  brevoApiKey: process.env.BREVO_API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
+  sessionSecret: process.env.SESSION_SECRET,
+  senderEmail: process.env.SENDER_EMAIL
+};
